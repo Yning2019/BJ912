c67d8d2fbd5ee753e707ef0b018a53fc7dc709db
yuanning <yn913310@163.com>
2019-09-12 11:27:55 +0800

    Initial commit

---
+++ scripts/test_001.py
@@ -0,0 +1,10 @@
+class Test001:
+    def test_001(self):
+        print("test001被执行")
+
+    def test_002(self):
+        print("test002被执行")
+
+    def test_003(self):
+        print("test003被执行")
+        assert 0
